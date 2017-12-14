145d1c1a1ba6eed31388f055f1112c64b788ac37
anshuman096 <anshuman096@gmail.com>
2017-12-13 23:45:41 -0600

    fix merge conflict

--- src/app.R
+++ src/app.R
@@ -10,7 +10,6 @@
 library(shiny)
 library(shinydashboard)
 library(shinythemes)
-<<<<<<< Updated upstream
 # Define UI for application that draws a histogram
 # library(dygraphs) # optional, used for dygraphs
 
@@ -22,53 +21,53 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem(text = "Bootstrap and Jackknife",  tabName = "bootknife"),
     menuItem(text="Monte Carlo Integration", tabName = "mci")
-    ) # /menuItem
-    # this is where other menuItems & menuSubItems would go
-  ) # /sidebarMenu
+  ) # /menuItem
+  # this is where other menuItems & menuSubItems would go
+) # /sidebarMenu
 # /dashboardSidebar
 
 #Body elements for the search visualizations.
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "mci",
-              # Sidebar with a slider input for number of bins 
-              sidebarLayout(
-                sidebarPanel(
-                  radioButtons("dist", "Distribution type:",
-                               c("Normal" = "norm",
-                                 "Uniform" = "unif",
-                                 "Log-normal" = "lnorm",
-                                 "Exponential" = "exp",
-                                 "Geometric" = "geo",
-                                 "Binomial" = "binom",
-                                 "Poisson" = "poisson")
-                  ),
-                  
-                  conditionalPanel("input.dist == 'geo'",
-                                   numericInput("geop","Enter the probability value:","")),
-                  conditionalPanel("input.dist == 'binom'",
-                                   numericInput("size", "Enter the size:","")),
-                  conditionalPanel("input.dist == 'binom'",
-                                   numericInput("bp","Enter the probability value:","")),
-                  conditionalPanel("input.dist == 'poisson'",
-                                   numericInput("lambda","Enter the lambda value:","")),
-                  
-                  
-                  # br() element to introduce extra vertical spacing ----
-                  br(),
-                  
-                  textInput("text", "Enter the function:"),
-                  numericInput("iterations", "Input the number of iterations:", "1"),
-                  numericInput("leftb", "Left Bound:", "0"),
-                  numericInput("rightb", "Right Bound:", "0")
+            # Sidebar with a slider input for number of bins 
+            sidebarLayout(
+              sidebarPanel(
+                radioButtons("dist", "Distribution type:",
+                             c("Normal" = "norm",
+                               "Uniform" = "unif",
+                               "Log-normal" = "lnorm",
+                               "Exponential" = "exp",
+                               "Geometric" = "geo",
+                               "Binomial" = "binom",
+                               "Poisson" = "poisson")
                 ),
-                mainPanel(
-                  verbatimTextOutput("text"),
-                  verbatimTextOutput("theta.hat"),
-                  verbatimTextOutput("actual"),
-                  plotOutput(outputId = "plot")
-                )
-        )
+                
+                conditionalPanel("input.dist == 'geo'",
+                                 numericInput("geop","Enter the probability value:","")),
+                conditionalPanel("input.dist == 'binom'",
+                                 numericInput("size", "Enter the size:","")),
+                conditionalPanel("input.dist == 'binom'",
+                                 numericInput("bp","Enter the probability value:","")),
+                conditionalPanel("input.dist == 'poisson'",
+                                 numericInput("lambda","Enter the lambda value:","")),
+                
+                
+                # br() element to introduce extra vertical spacing ----
+                br(),
+                
+                textInput("text", "Enter the function:"),
+                numericInput("iterations", "Input the number of iterations:", "1"),
+                numericInput("leftb", "Left Bound:", "0"),
+                numericInput("rightb", "Right Bound:", "0")
+              ),
+              mainPanel(
+                verbatimTextOutput("text"),
+                verbatimTextOutput("theta.hat"),
+                verbatimTextOutput("actual"),
+                plotOutput(outputId = "plot")
+              )
+            )
     ),
     tabItem(tabName = "bootknife",
             # Sidebar with a slider input for number of bins 
@@ -102,16 +101,13 @@ body <- dashboardBody(
 ) # /dashboardBody
 
 dashboardPage(header, sidebar, body, skin = "black")
-=======
-library(ggplot2)
->>>>>>> Stashed changes
 
 ui <- fluidPage(
   
-   theme = shinytheme("slate"),
-   # Application title
-   titlePanel("Monte Carlo Integration Calculator"),
-   dashboardPage(header, sidebar, body, skin = "black")
+  theme = shinytheme("slate"),
+  # Application title
+  titlePanel("Monte Carlo Integration Calculator"),
+  dashboardPage(header, sidebar, body, skin = "black")
   
 )
 
@@ -170,139 +166,143 @@ formatCI = function(ci_obj) {
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$text <- renderText({
-     paste("Function:", input$text)
-     
-   })
-   
-   formula = function(){
-     req(input$iterations)
-     req(input$dist)
-
-     
-     m=input$iterations
-     left_bound = input$leftb
-     right_bound = input$rightb
-     ndist=input$dist
-     ngeop=input$geop
-     nsize=input$size
-     nbp=input$bp
-     nlambda=input$lambda
-     
-     if(ndist == "unif")({
-       x=runif(m, min = left_bound, max = right_bound)
-     })
-     else if(ndist == "norm")({
-       x=rnorm(m, left_bound, right_bound)
-     })
-     else if(ndist == "geo")({
-       x=rgeom(m, ngeop) #n,prob
-     })
-     else if(ndist == "binom")({
-       x=rbinom(m,nsize,nbp) #n,size,prob
-     })
-     else if(ndist == "exp")({
-       x=rexp(m)
-     })
-     else if(ndist == "poisson")({
-       x=rpois(m,nlambda) #n,lambda
-     })
-     else if(ndist == "lnorm")({
-       x=rlnorm(m)
-     })
-     
-     req(input$text)
-     req(input$rightb)
-     req(input$leftb)
-     
-     ntext <- input$text
-     nrightb <- parse(text=input$rightb)
-     nleftb <- parse(text=input$leftb)
-     nntext = parse(text = ntext)
-     mean(eval(nntext))*(eval(nrightb)-eval(nleftb))
-     
-   }
   
-   
-   integral = function(){
-     req(input$text) # require the text for the function to integrate
-     req(input$rightb) # upper bound
-     req(input$leftb) # lower bound
-     
-     nrightb <- parse(text=input$rightb) # right bound
-     nleftb <- parse(text=input$leftb) # left bound
-     #nntext = parse(text = ntext) # converts text to expression
-     integrand <- function(x){
-       eval(parse(text = input$text)) # 
-     }
-     val = integrate(integrand, lower = eval(nleftb), upper = eval(nrightb))
-     if(is.nan(val[1]))
-      print("integral diverges or cannot be evaluated")
-     else
-      val[1]
-   }
-   
-   output$theta.hat = renderText({
-     paste("Estimate:", formula())
-   })
-   
-   output$actual = renderText({
-     paste("Actual:", integral())
-   })
-   
-   
-   output$plot = renderPlot({
-     actual_value = integral() # actual value of the integral
-     simulation_data = vector(length = input$iterations)
-     for(i in 1:length(simulation_data))
-       simulation_data[i] = formula()
-     
-     estimation_data = data.frame(seq(1:input$iterations), simulation_data)
-     names(estimation_data) = c("Simulation", "Value")
-     
-     values_plot = ggplot(estimation_data) +
-       geom_point(aes(Simulation, Value), color = "blue") + 
-       geom_hline(yintercept = as.numeric(actual_value[1]), color = "red") + 
-       labs(title = "Scatterplot of Parameter Estimations", x = "Simulation", y = "Value")
-     
-     values_plot
-   })
-   
-   
-   dt = reactive ({
-     getData(input$repSize, input$estimator)
-   })
-   
-   output$distPlot <- renderPlot({
-     data = dt()$dt
-     ci = dt()$ci
-     #hist(data$t, col='darkgray', border = 'white', main ="Histogram of the Estimator", xlab = "Estimator" )
-     res = data.frame(data$t)
-     ggplot(res) + geom_histogram() + xlab("Estimator")
-     #abline(v=mean(data$t), col="red")
-     #abline(v=median(data$t), col="black", lty=2)
-     #legend("topleft", 
-     #      c("mean", "median"),
-     #     lty=c(1, 2), 
-     #    col=c("red","black"), 
-     #   bty = "n")
-   })
-   
-   output$stats = renderPrint({
-     data = dt()$dt
-     summary(data$t)
-     
-   })
-   
-   output$CI = renderPrint({
-     estimator = dt()$dt
-     ci_types = input$ci
-     boot.ci(estimator, type=ci_types)
-   })
-   
+  output$text <- renderText({
+    paste("Function:", input$text)
+    
+  })
+  formula = function(){
+    req(input$iterations)
+    req(input$dist)
+    
+    
+    m=input$iterations
+    left_bound = input$leftb
+    right_bound = input$rightb
+    ndist=input$dist
+    ngeop=input$geop
+    nsize=input$size
+    nbp=input$bp
+    nlambda=input$lambda
+    
+    if(ndist == "unif")({
+      x=runif(m, min = left_bound, max = right_bound)
+    })
+    else if(ndist == "norm")({
+      x=rnorm(m, left_bound, right_bound)
+    })
+    else if(ndist == "geo")({
+      x=rgeom(m, ngeop) #n,prob
+    })
+    else if(ndist == "binom")({
+      x=rbinom(m,nsize,nbp) #n,size,prob
+    })
+    else if(ndist == "exp")({
+      x=rexp(m)
+    })
+    else if(ndist == "poisson")({
+      x=rpois(m,nlambda) #n,lambda
+    })
+    else if(ndist == "lnorm")({
+      x=rlnorm(m)
+    })
+    
+    req(input$text)
+    req(input$rightb)
+    req(input$leftb)
+    
+    ntext <- input$text
+    nrightb <- parse(text=input$rightb)
+    nleftb <- parse(text=input$leftb)
+    nntext = parse(text = ntext)
+    mean(eval(nntext))*(eval(nrightb)-eval(nleftb))
+    
+  }
+  
+  integral = function(){
+    req(input$text)
+    req(input$rightb)
+    req(input$leftb)
+    req(input$iterations)
+    req(input$dist)
+    
+    m=input$iterations
+    ndist=input$dist
+    ngeop=input$geop
+    nsize=input$size
+    nbp=input$bp
+    nlambda=input$lambda
+    
+    ntext <- input$text
+    nrightb <- parse(text=input$rightb)
+    nleftb <- parse(text=input$leftb)
+    nntext = parse(text = ntext)
+    integrand <- function(x){ eval(parse(text = input$text))}
+    #Mu1b <- Vectorize(integrand, "x")
+    val = integrate(integrand, lower = eval(nleftb), upper = eval(nrightb))
+    val[1]
+  }
+  
+  output$theta.hat = renderText({
+    paste("Estimate:", formula())
+  })
+  
+  output$actual = renderText({
+    paste("Actual:", integral())
+  })
+  
+  library(ggplot2)
+  output$plot = renderPlot({
+    actual_value = integral() # actual value of the integral
+    simulation_data = vector(length = input$iterations)
+    for(i in 1:length(simulation_data))
+      simulation_data[i] = formula()
+    
+    estimation_data = data.frame(seq(1:input$iterations), simulation_data)
+    names(estimation_data) = c("Simulation", "Value")
+    # print(estimation_data)
+    
+    values_plot = ggplot(estimation_data) +
+      geom_point(aes(Simulation, Value), color = "blue") + 
+      geom_hline(yintercept = as.numeric(actual_value[1]), color = "red") + 
+      labs(title = "Scatterplot of Parameter Estimations", x = "Simulation", y = "Value")
+    
+    values_plot
+  })
+  
+  
+  dt = reactive ({
+    getData(input$repSize, input$estimator)
+  })
+  
+  output$distPlot <- renderPlot({
+    data = dt()$dt
+    ci = dt()$ci
+    #hist(data$t, col='darkgray', border = 'white', main ="Histogram of the Estimator", xlab = "Estimator" )
+    res = data.frame(data$t)
+    ggplot(res) + geom_histogram() + xlab("Estimator")
+    #abline(v=mean(data$t), col="red")
+    #abline(v=median(data$t), col="black", lty=2)
+    #legend("topleft", 
+    #      c("mean", "median"),
+    #     lty=c(1, 2), 
+    #    col=c("red","black"), 
+    #   bty = "n")
+  })
+  
+  output$stats = renderPrint({
+    data = dt()$dt
+    summary(data$t)
+    
+  })
+  
+  output$CI = renderPrint({
+    estimator = dt()$dt
+    ci_types = input$ci
+    boot.ci(estimator, type=ci_types)
+  })
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
